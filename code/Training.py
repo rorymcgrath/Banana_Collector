07dc45bf1710922867d430e4014774a0856eead5
Rory McGrath <rory.m.mc.grath@accenture.com>
2019-01-29 18:16:12 +0000

    Added comments to Traing code to explain the process.

--- code/Training.py
+++ code/Training.py
@@ -5,17 +5,20 @@ from collections import deque
 import pickle
 import torch
 
+
+#Define the maximum number of time steps in each episode.
 MAX_T = 277 
+#Define how epsilong will decay during training
 EPS_START = 1.0
 EPS_END = 0.01
 EPS_DECAY = 0.995
 
+#Initlise the environment, np_graphics are set to true to disable UI window.
 env = UnityEnvironment(file_name="../Banana.app",no_graphics=True)
-
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 
-
+#Initlise the environment for training
 env_info = env.reset(train_mode=True)[brain_name]
 action_size = brain.vector_action_space_size
 state = env_info.vector_observations[0]
@@ -31,6 +34,7 @@ scores_window = deque(maxlen=100)
 scores = []
 
 eps = EPS_START
+#Define the minimum aver score for success.
 success_score = 13
 for i_episode in range(1,1500):
 	env_info = env.reset(train_mode=True)[brain_name]
@@ -56,10 +60,13 @@ for i_episode in range(1,1500):
 		print('\rEpisode {} \tAverage Score: {:.2f} \tEpsilon: {:.5f}'.format(i_episode, np.mean(scores_window),eps))
 	
 	if np.mean(scores_window) >= success_score:
+		#If the average score is greater then the current success score save the model paremeters and increase the success score by 1
+		#Given that the average score is being reposed over 100 episodes the environment was solved 100 episodes previously at the start of this rolling window.
 		print('Environment solved in {:d} episodes. Average Score: {:.2f} Saving model parameters.'.format(i_episode-100,np.mean(scores_window)))
 		torch.save(agent.local_qnetwork.state_dict(), 'checkpoint.pth')
 		success_score+=1
 
 with open('scores.pkl','wb') as f:
+	#Save the scores so that they can be visualised later.
 	pickle.dump(scores,f)
 
