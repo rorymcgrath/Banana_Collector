cfd90784e9bf22a0da44870294e5ac5a4e1ae500
Rory McGrath <rory.m.mc.grath@accenture.com>
2019-01-30 16:11:54 +0000

    Added saved scores and script to generate result plot.

---
+++ code/PlotStatistics.py
@@ -0,0 +1,16 @@
+import pickle
+import matplotlib 
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('scores.pkl','rb') as s:
+	scores = pickle.load(s)
+
+rewards, = plt.plot([i for i in range(len(scores))],scores,label='Reward')
+average_rewards, = plt.plot([i for i in range(100,len(scores))],[np.mean(scores[i-100:i]) for i in range(100,len(scores))], '--r', label='Averge Reward')
+plt.xlabel('Episode Number')
+plt.ylabel('Reward')
+plt.legend(handles=[rewards,average_rewards])
+plt.grid(True)
+plt.title('Results')
+plt.show()
